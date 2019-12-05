ff7a17851dc76fe8e7ad7703f454f9fde6221920
Aman Modi <aman@shoptrade.co>
2019-12-05 13:02:46 +0530

    First Commit

---
+++ custom.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $('.js-case-studies-carousel').slick({
+        slidesToShow: 2,
+        arrows: true,
+        slidesToScroll: 1,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 2,
+                    infinite: true,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    infinite: true,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function(){
+    $('.js-skeptics-speak-image-carousel').slick({
+        slidesToShow: 2,
+        arrows: true,
+        slidesToScroll: 1,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 3,
+                    infinite: true,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    infinite: true,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+});
+
+
+
+                        
