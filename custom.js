318835f12530a395cadb786292024f863ac3e003
Aman Modi <aman@shoptrade.co>
2019-12-18 19:19:36 +0530

    Hamburger menu implemented for ipad and for mobile a few changes have to be made

--- custom.js
+++ custom.js
@@ -136,8 +136,10 @@ then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
 });
 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
-
-
-
-                        
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
